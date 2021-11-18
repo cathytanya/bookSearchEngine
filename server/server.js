d5725d2490650923fa60e9d393ab8a60dad00a70
cathytanya <cathycolette.tanya@gmail.com>
2021-11-17 22:24:34 -0500

    use ApolloServer with express

--- server/server.js
+++ server/server.js
@@ -20,6 +20,9 @@ const server = new ApolloServer({
   context: authMiddleware
 })
 
+// use ApolloServer with express
+server.applyMiddleware({app})
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
