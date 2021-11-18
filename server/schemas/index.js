d46589b42998d6bd4d61234143990beaa885161f
cathytanya <cathycolette.tanya@gmail.com>
2021-11-17 22:35:29 -0500

    import and export typeDefs and resolvers in the index.js

---
+++ server/schemas/index.js
@@ -0,0 +1,6 @@
+// import typeDefs and resolvers
+const resolvers = require('./resolvers');
+const typeDefs = require('./typeDefs');
+
+// export typeDefs and resolvers
+module.exports = {resolvers,typeDefs}
