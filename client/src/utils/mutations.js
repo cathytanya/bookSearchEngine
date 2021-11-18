2d995643d3efa7664f91c47d5d0c652ccdea7e33
cathytanya <cathycolette.tanya@gmail.com>
2021-11-18 02:04:47 -0500

    edit the mutations.js file

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,56 +1,58 @@
-import { gql } from '@apollo/client';
+import gql from 'graphql-tag';
 
 export const LOGIN_USER = gql`
-  mutation login($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
-      token
-      user {
-        _id
-        username
-      }
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
     }
-  }
 `;
 
 export const ADD_USER = gql`
-  mutation addUser($username: String!, $email: String!, $password: String!) {
-    addUser(username: $username, email: $email, password: $password) {
-      token
-      user {
-        _id
-        username
-      }
+    mutation addUser($username: String!, $email: String!, $password: String!){
+        addUser(username: $username, email: $email, password: $password){
+            token
+            user{
+                _id
+                username
+            }
+        }
     }
-  }
 `;
 
-export const ADD_THOUGHT = gql`
-  mutation addThought($thoughtText: String!) {
-    addThought(thoughtText: $thoughtText) {
-      _id
-      thoughtText
-      thoughtAuthor
-      createdAt
-      comments {
-        _id
-        commentText
-      }
+export const SAVE_BOOK = gql`
+    mutation saveBook($authors: [String], $description: String, $title: String, $bookId: String, $image: String, $link: String){
+        saveBook(authors: $authors, description: $description, title: $title, bookId: $bookId, image: $image, link: $link){
+            _id
+            username
+            savedBooks{
+                authors
+                description
+                title
+                bookId
+                image
+                link
+            }
+        }
     }
-  }
 `;
 
-export const ADD_COMMENT = gql`
-  mutation addComment($thoughtId: ID!, $commentText: String!) {
-    addComment(thoughtId: $thoughtId, commentText: $commentText) {
-      _id
-      thoughtText
-      thoughtAuthor
-      createdAt
-      comments {
-        _id
-        commentText
-        createdAt
-      }
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId:String!){
+        removeBook(bookId: $bookId){
+            username
+            savedBooks{
+                authors
+                description
+                title
+                bookId
+                image
+                link
+            }
+        }
     }
-  }
 `;
