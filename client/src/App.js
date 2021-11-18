4785f6a7b36143abc1711f4e6cea79bfd9d83bfe
cathytanya <cathycolette.tanya@gmail.com>
2021-11-18 00:19:35 -0500

    import apolloProvider and apolloClient

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,8 @@
 import React from 'react';
+// import apollo provider
+import {ApolloProvider} from '@apollo/react-hooks'
+// import ApolloClient
+import ApolloClient from 'apollo-boost';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
