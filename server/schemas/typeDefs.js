e9d7d1376374fcc68ec4f4958a2318e14ddc4063
cathytanya <cathycolette.tanya@gmail.com>
2021-11-17 23:33:29 -0500

    add the data type for the book,user and auth in the type def

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,40 @@
+// import the SQL tagged template function
+const {sql} =require('apollo-server-express');
+
+const typeDefs = gql`
+  type Book {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: int
+    savedBooks: [Book]
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    users: [User]
+    me: User
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    removeBook(bookId: String): User
+    saveBook(bookId: String, authors: [String], description: String, title: String, image: String, link: String): User
+  }
+`;
+
+module.exports = typeDefs;
